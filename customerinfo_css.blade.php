5ec533be789e163af5b1fda1830a2e375b65e97e
rafanthony <121413490+rafanthony@users.noreply.github.com>
2023-04-23 22:42:09 +0800

    Create customerinfo_css.blade.php

---
+++ customerinfo_css.blade.php
@@ -0,0 +1,44 @@
+<style>
+   
+    #CREATE_BP_PAGE-cont{
+		overflow-y:hidden !important;
+	}
+
+	.sapMInputBase .sapMInputBaseDisabledWrapper {
+    	border: 1px transparent;
+    	background: #fff;
+	}
+
+	.sapMInputBaseReadonly .sapMInputBaseContentWrapper {
+		border-color: #ccc;
+		background: none;
+		/* background-color: rgba(242,242,242,0.5); */
+		border: 1px transparent;
+	}
+
+	.sapUiLocalBusyIndicator:focus:after {
+		content: "";
+		position: absolute;
+		left: 1px;
+		top: 1px;
+		right: 1px;
+		bottom: 1px;
+		border: 2px solid transparent;
+		pointer-events: none
+	}
+	.sapUiSizeCompact .sapMTB.sapMTBHeader-CTX {
+		height: 30px;
+	}
+
+	#BP_GENERAL_PANEL .sapUiFormResLayout>div {
+    	padding: 50px;
+    	box-sizing: border-box;
+	}
+
+	.sapMSlt.sapMSltReadonly {
+		border-color: #ccc;
+		background: transparent;
+		border: 1px transparent;
+	}
+    
+</style>
